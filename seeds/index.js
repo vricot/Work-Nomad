61304844882ef90a91af4e269c8869385b8a4da7
Valerie Ricot <valeriericot@gmail.com>
2021-02-12 17:29:18 -0500

    Created a test worksite

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Worksite = require('./models/worksite');
+const Worksite = require('../models/worksite');
 
 mongoose.connect('mongodb://localhost:27017/work-nomad', {
     useNewUrlParser: true,
@@ -12,3 +12,11 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () =>{
     console.log("Database connected");
 });
+//Check why this function won't run:
+const seedDB = async () => {
+    await Worksites.deleteMany({});
+    const w = new Worksite({ title: 'warm coffee' });
+    await w.save();
+}
+
+seedDB();
