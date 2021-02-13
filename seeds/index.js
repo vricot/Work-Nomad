117ae7f7b1c20da34ef3b241256d0da449d53d33
Valerie Ricot <valeriericot@gmail.com>
2021-02-13 12:30:00 -0500

    Added functions to randomly generate worksite names and city/states

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
 const Worksite = require('../models/worksite');
+
 
 mongoose.connect('mongodb://localhost:27017/work-nomad', {
     useNewUrlParser: true,
@@ -12,11 +15,21 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () =>{
     console.log("Database connected");
 });
-//Check why this function won't run:
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
-    await Worksites.deleteMany({});
-    const w = new Worksite({ title: 'warm coffee' });
-    await w.save();
+    await Worksite.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const worksite = new Worksite({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await worksite.save();
+    }
 }
 
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+})
