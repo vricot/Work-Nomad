e2a5609a608304ef5af5689d278126ba2d15b608
Valerie Ricot <valeriericot@gmail.com>
2021-02-13 13:36:25 -0500

    Added post request option to add new workspots

--- app.js
+++ app.js
@@ -21,19 +21,32 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
 app.get('/workspots', async (req, res) => {
     const allWorkspots = await Workspot.find({});
     res.render('workspots/index', { allWorkspots })
+});
+
+app.get('/workspots/new', (req, res) => {
+    res.render('workspots/new');
+});
+
+app.post('/workspots', async (req, res) => {
+    const workspot = new Workspot(req.body.workspot);
+    await workspot.save();
+    res.redirect(`/workspots/${workspot._id}`)
 })
 
 app.get('/workspots/:id', async (req, res) => {
     const workspot = await Workspot.findById(req.params.id)
     res.render('workspots/show', { workspot });
-})
+});
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
