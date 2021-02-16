914557a416c9314b4e6f54621838d9123bcb8098
Valerie Ricot <valeriericot@gmail.com>
2021-02-16 14:03:37 -0500

    Added image key to schema; changed 'Worksite' to 'Workspot'

--- models/workspot.js
+++ models/workspot.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const WorkspotSchema = new Schema({
     title: String,
+    image: String,
     hours: String,
     priceRange: String,
     description: String,
     location: String
 });
 
-module.exports = mongoose.model('Worksite', WorkspotSchema);
+module.exports = mongoose.model('Workspot', WorkspotSchema);
