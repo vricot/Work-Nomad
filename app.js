0ac9cfb6a4d9e327583bef8b3c72f4e741d77bc0
Valerie Ricot <valeriericot@gmail.com>
2021-02-17 17:21:56 -0500

    Wrapped every async function around an async error catch handler; updated default error message and status code

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utilities/catchAsync');
+const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
 const Workspot = require('./models/workspot');
+const { stat } = require('fs');
 
 mongoose.connect('mongodb://localhost:27017/work-nomad', {
     useNewUrlParser: true,
@@ -31,47 +33,54 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/workspots', async (req, res) => {
+app.get('/workspots', catchAsync(async (req, res) => {
     const allWorkspots = await Workspot.find({});
     res.render('workspots/index', { allWorkspots })
-});
+}));
 
 app.get('/workspots/new', (req, res) => {
     res.render('workspots/new');
 });
 
 app.post('/workspots', catchAsync(async (req, res, next) => {
+    if(!req.body.workspot) throw new ExpressError('Invalid Workspot Data', 400)
     const workspot = new Workspot(req.body.workspot);
     await workspot.save();
     res.redirect(`/workspots/${workspot._id}`)
 }))
 
-app.get('/workspots/:id', async (req, res) => {
+app.get('/workspots/:id', catchAsync(async (req, res) => {
     const workspot = await Workspot.findById(req.params.id)
     res.render('workspots/show', { workspot });
-});
+}));
 
-app.get('/workspots/:id/edit', async (req, res) => {
+app.get('/workspots/:id/edit', catchAsync(async (req, res) => {
     const workspot = await Workspot.findById(req.params.id)
     res.render('workspots/edit', { workspot });
-});
+}));
 
-app.put('/workspots/:id', async (req, res) => {
+app.put('/workspots/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const workspot = await Workspot.findByIdAndUpdate(id, { ...req.body.workspot });
     res.redirect(`/workspots/${workspot._id}`);
-});
+}));
 
-app.delete('/workspots/:id', async (req, res) => {
+app.delete('/workspots/:id', catchAsync(async (req, res) => {
      const { id } = req.params;
      await Workspot.findByIdAndDelete(id);
      res.redirect('/workspots');
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 });
 
 app.use((err, req, res, next) => {
-    res.send('Oh no, something went wrong!')
-})
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh No! Something Went Wrong!'
+    res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
-})
+});
