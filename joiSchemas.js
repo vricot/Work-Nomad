b7b8daefdae2ee347ed6ad3368a28c40c92c8d3e
Valerie Ricot <valeriericot@gmail.com>
2021-02-18 16:49:31 -0500

    Added review validation model

--- joiSchemas.js
+++ joiSchemas.js
@@ -8,6 +8,12 @@ module.exports.workspotSchema = Joi.object({
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
-    }).required(),
+    }).required()
 });
 
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+})
