863619eb99cca3545f38c905a572ce493d2f23e4
Valerie Ricot <valeriericot@gmail.com>
2021-02-19 14:55:41 -0500

    Initial Commit

---
+++ routes/reviews.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const Workspot = require('../models/workspot');
+const Review = require('../models/review');
+
+const { reviewSchema } = require('../joiSchemas');
+
+const catchAsync = require('../utilities/catchAsync');
+const ExpressError = require('../utilities/ExpressError');
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+router.post('/', validateReview, catchAsync(async (req, res) => {
+    const workspot = await Workspot.findById(req.params.id);const review = new Review(req.body.review);
+    workspot.reviews.push(review);
+    await review.save();
+    await workspot.save();
+    res.redirect(`/workspots/${workspot._id}`);
+ }));
+ 
+ router.delete('/:reviewId', catchAsync(async (req, res) => {
+     const { id, reviewId } = req.params;
+     await Workspot.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+     await Review.findByIdAndDelete(reviewId);
+     res.redirect(`/workspots/${id}`);
+ }));
+
+ module.exports = router;
