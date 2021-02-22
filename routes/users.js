381c49da9dbaeedc13bb044ce59c86ff38033229
Valerie Ricot <valeriericot@gmail.com>
2021-02-22 12:12:32 -0500

    Added Login route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
@@ -19,5 +20,14 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/workspots');
+})
 
 module.exports = router;
