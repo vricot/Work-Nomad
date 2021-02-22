a66e1aaa1294282e48edcb4cf69348fc1d4ad5f3
Valerie Ricot <valeriericot@gmail.com>
2021-02-22 13:12:16 -0500

    Added middleware that stores the session of the user they can return to after registering/logging in

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
  module.exports.isLoggedIn = (req, res, next) => {
      console.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be logged in');
         return res.redirect('/login');
     }
