25e16416b96335e6b7dd23303868ca5acb33db4e
Valerie Ricot <valeriericot@gmail.com>
2021-02-22 12:37:11 -0500

    Initial Commit

---
+++ middleware.js
@@ -0,0 +1,14 @@
+ module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'You must be logged in');
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
+
+
+
+
+
