16c17b39fca81f23258868052db7bc930a6f348f
Valerie Ricot <valeriericot@gmail.com>
2021-02-22 12:54:10 -0500

    Added function to check if user is logged in and display message/redirect accordingly

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
  module.exports.isLoggedIn = (req, res, next) => {
+     console.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be logged in');
         return res.redirect('/login');
