2453283f5f6c7f9f4b75fbb8e2f2c3a1f038e09c
Valerie Ricot <valeriericot@gmail.com>
2021-02-23 12:38:07 -0500

    Moved review validation out into middleware file; added flash success message for addingn new review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-
+const { validateReview } = require('../middleware')
 const Workspot = require('../models/workspot');
 const Review = require('../models/review');
 
-const { reviewSchema } = require('../joiSchemas');
-
 const catchAsync = require('../utilities/catchAsync');
 const ExpressError = require('../utilities/ExpressError');
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
     const workspot = await Workspot.findById(req.params.id);const review = new Review(req.body.review);
     workspot.reviews.push(review);
     await review.save();
     await workspot.save();
+    req.flash('success', 'Successfully added new review!');
     res.redirect(`/workspots/${workspot._id}`);
  }));
  
@@ -31,7 +20,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
      const { id, reviewId } = req.params;
      await Workspot.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
      await Review.findByIdAndDelete(reviewId);
-     req.flash('success', 'Successfully deleted review');
+     req.flash('success', 'Successfully deleted review!');
      res.redirect(`/workspots/${id}`);
  }));
 
