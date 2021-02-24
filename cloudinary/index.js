30b7ce6d74cb5b40337ae2a6e71dc7e3220a02ec
Valerie Ricot <valeriericot@gmail.com>
2021-02-24 12:44:20 -0500

    Added cloudinary configuration settings

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,2 +1,19 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({
+    cloud_name: process.env.ClOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    folder: 'WorkNomad',
+    allowedFormats: ['jpeg', 'png', 'jpg']
+});
+
+module.exports = {
+    cloudinary,
+    storage
+}
