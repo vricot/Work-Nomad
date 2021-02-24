085fffcf153fb2c9ea168c29eea1ea6cf8e3a10b
Valerie Ricot <valeriericot@gmail.com>
2021-02-24 12:44:49 -0500

    Imported cloudinary and multer for image storage purposes

--- routes/workspots.js
+++ routes/workspots.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const workspots = require('../controllers/workspots');
 const catchAsync = require('../utilities/catchAsync');
 const { isLoggedIn, isAuthor, validateWorkspot } = require('../middleware');
+const multer  = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const Workspot = require('../models/workspot');
 
 router.route('/')
     .get(catchAsync(workspots.index))
-    .post(isLoggedIn, validateWorkspot, catchAsync(workspots.createWorkspot));
+    // .post(isLoggedIn, validateWorkspot, catchAsync(workspots.createWorkspot));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("IT WORKEDDDD!!!")
+    })
 
 router.get('/new', isLoggedIn, workspots.renderNewForm);
 
