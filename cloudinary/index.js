c4aa5498ca9ee91665d56adbeb238b0bedbaf199
Valerie Ricot <valeriericot@gmail.com>
2021-02-24 13:07:13 -0500

    Fixed typo

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.ClOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
