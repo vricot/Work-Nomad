4f610ba3141d2cea1e5e89c42395d824693f31cd
Valerie Ricot <valeriericot@gmail.com>
2021-02-24 15:21:40 -0500

    Nested 'folder' and 'allowed formats' within cloudinary storage params

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary,
-    folder: 'WorkNomad',
-    allowedFormats: ['jpeg', 'png', 'jpg']
+    params: {
+        folder: 'WorkNomad',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
 });
 
 module.exports = {
