206b648ae12ee1a6409f834c69b7e5b326f19eec
Valerie Ricot <valeriericot@gmail.com>
2021-02-24 16:59:49 -0500

    Used virtual to only display image thumbnail size width of 200

--- models/workspot.js
+++ models/workspot.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema ({
+        url: String,
+        filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+})
+
 const WorkspotSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     hours: String,
     price: Number,
     description: String,
