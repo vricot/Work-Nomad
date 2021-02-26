47f17be188f6f8dd2611cea4343222cafbb18df6
Valerie Ricot <valeriericot@gmail.com>
2021-02-26 16:10:51 -0500

    Added express mongo sanitize to prevent mongo injection

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const { stat } = require('fs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users')
 const workspotRoutes = require('./routes/workspots');
@@ -43,6 +44,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
