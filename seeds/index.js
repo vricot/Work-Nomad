675f20517f2d4b7fd068cffcf62733217a133050
Valerie Ricot <valeriericot@gmail.com>
2021-03-05 13:46:06 -0500

    Switched mongo connection for production

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
-const { dollarsigns } = require('./dollarRange');
 const Workspot = require('../models/workspot');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/work-nomad';
 
-mongoose.connect('mongodb://localhost:27017/work-nomad', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
